9b8398d1b4be58b5936ae51431a731208e9564d2
Otto Hajdu <raythekeno@gmail.com>
2023-01-06 16:44:52 +0100

    Add Button to Episode List

--- src/EpisodeMini.js
+++ src/EpisodeMini.js
@@ -1,12 +1,14 @@
 import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
 
-function EpisodeMini({name, air_date, episode}) {
+function EpisodeMini({id, name, air_date, episode}) {
   return (
   
     <tr>
       <th>{name}</th>
       <th>{episode}</th>
       <th>{air_date}</th>
+      <th><Link to={`/episode/${id}`} class="btn btn-outline-dark m-2">Page Episode</Link></th>
     </tr>
 
   );
